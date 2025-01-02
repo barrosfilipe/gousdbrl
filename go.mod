97debaf84fc607bbb1d60209a3397b5091dfb509
Filipe Barros <filipebarrossi@gmail.com>
2025-01-02 18:06:50 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/barrosfilipe/gousdbrl
+
+go 1.23
+
+toolchain go1.23.4
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.1
+	github.com/briandowns/spinner v1.23.1
+	github.com/fatih/color v1.18.0
+	github.com/tidwall/gjson v1.18.0
+	github.com/tidwall/sjson v1.2.5
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+const (
+	fileName         = "data.json"
+	wiseCurrencyPage = "https://wise.com/gb/currency-converter/usd-to-brl-rate?amount=1"
+)
+
+type Config struct {
+	Value float64
+}
+
+func loadConfig() (*Config, error) {
+	var config Config
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &Config{Value: 0.0}, nil
+		}
+		return nil, err
+	}
+
+	value := gjson.Get(string(data), "config.value")
+	if !value.Exists() {
+		return nil, fmt.Errorf("key 'config.value' not found in config file")
+	}
+
+	config.Value = value.Float()
+	return &config, nil
+}
+
+func saveConfig(config *Config) error {
+	data := "{}"
+	if _, err := os.Stat(fileName); err == nil {
+		fileData, _ := os.ReadFile(fileName)
+		data = string(fileData)
+	}
+
+	newData, err := sjson.Set(data, "config.value", config.Value)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, []byte(newData), 0644)
+}
+
+func fetchExchangeRate(url string, resultChan chan<- string, errChan chan<- error) {
+	s := spinner.New(spinner.CharSets[39], 100*time.Millisecond)
+	s.Start()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		errChan <- fmt.Errorf("failed to fetch the page: %w", err)
+		s.Stop()
+		return
+	}
+	defer resp.Body.Close()
+
+	s.Stop()
+
+	if resp.StatusCode != http.StatusOK {
+		errChan <- fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		errChan <- fmt.Errorf("failed to parse HTML: %w", err)
+		return
+	}
+
+	rate := doc.Find("span[dir='ltr'] span.text-success").Text()
+	if rate == "" {
+		errChan <- fmt.Errorf("exchange rate not found in HTML")
+		return
+	}
+
+	resultChan <- rate
+}
+
+func handleRateComparison(rateStr string, configValue float64) (string, error) {
+	rateFloat, err := strconv.ParseFloat(rateStr, 64)
+	if err != nil {
+		return "", fmt.Errorf("error parsing rate: %w", err)
+	}
+
+	arrowIcon := ""
+
+	switch {
+	case rateFloat < configValue:
+		arrowIcon = color.New(color.Bold, color.FgHiRed).Sprint("▼ ")
+	case rateFloat > configValue:
+		arrowIcon = color.New(color.Bold, color.FgHiGreen).Sprint("▲ ")
+	default:
+		arrowIcon = color.New(color.Bold, color.FgHiWhite).Sprint("▶ ")
+	}
+
+	return arrowIcon + color.New(color.Bold, color.FgYellow).Sprint(rateStr), nil
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	loading := color.New(color.Bold, color.FgHiBlue).Sprint("Fetching USD to BRL exchange rate from ")
+	highlight := color.New(color.Bold, color.FgGreen).Sprint("Wise")
+	fmt.Println(loading + highlight)
+
+	resultChan := make(chan string)
+	errChan := make(chan error)
+
+	go fetchExchangeRate(wiseCurrencyPage, resultChan, errChan)
+
+	var config *Config
+	var err error
+	config, err = loadConfig()
+	if err != nil {
+		color.Red("Error loading config: %v\n", err)
+		log.Fatalf("Error loading config: %v\n", err)
+	}
+
+	select {
+	case rate := <-resultChan:
+		arrowMessage, err := handleRateComparison(rate, config.Value)
+		if err != nil {
+			color.Red("Error handling rate comparison: %v\n", err)
+			log.Fatalf("Error handling rate comparison: %v\n", err)
+		}
+
+		config.Value, err = strconv.ParseFloat(rate, 64)
+		if err != nil {
+			color.Red("Error updating config value: %v\n", err)
+			log.Fatalf("Error updating config value: %v\n", err)
+		}
+
+		err = saveConfig(config)
+		if err != nil {
+			color.Red("Error saving config: %v\n", err)
+			log.Fatalf("Error saving config: %v\n", err)
+		}
+
+		fmt.Println(arrowMessage)
+	case err := <-errChan:
+		color.Red("Error: %v\n", err)
+		log.Fatalf("Error: %v\n", err)
+	}
+}
